37a4bc2cd3560856ed03419ba2c85b9a017d9ab4
Shaun Russell <shaundavidrussell@yahoo.com>
2016-08-26 11:12:25 -0500

    Fixed submenu arrow animation for Firefox

--- dist/js/app.js
+++ dist/js/app.js
@@ -8,9 +8,11 @@ $('.submenu-icon').on('click', function() {
 	
 	var $submenu = $(this).parent().siblings('.submenu'),
 	    pointingRight = 'matrix(-1, 1.22465e-16, -1.22465e-16, -1, 0, 0)',
+		MOZpointingRight = 'matrix(0, -1, 1, 0, 0, 0)',
 		pointingDown = 'matrix(-1.83697e-16, -1, 1, -1.83697e-16, 0, 0)',
 		$svgClicked = $submenu.siblings('div.tab-cont').find('svg'),
 	    $transformValue = $svgClicked.css('transform');
+		$MOZtransformValue = $svgClicked.css('-moz-transform');
 
 	$('.submenu').each(function() {
 				
@@ -21,25 +23,25 @@ $('.submenu-icon').on('click', function() {
 			$(this).slideUp();
 			$(this).siblings('div.tab-cont').find('svg').css({
 				'transform'      : pointingRight,
-				'-moz-transform' : "rotate(270deg)"
+				'-moz-transform' : MOZpointingRight
 				})
 		}
 	});
 	
 	$submenu.slideToggle();
-	
+		console.log($MOZtransformValue);
+		console.log(MOZpointingRight);
 
-	if( pointingRight === $transformValue ) {
+	if( pointingRight === $transformValue || MOZpointingRight === $MOZtransformValue) {
 		
 		$transformValue = $svgClicked.css({
 			'transform'      : pointingDown,
-			'-moz-transform' : "rotate(315deg)"
+			'-moz-transform' : 'rotate(360deg)'
 			});
 	} else {
-		
 		$transformValue = $svgClicked.css({
 			'transform'      : pointingRight,
-			'-moz-transform' : "rotate(270deg)"
+			'-moz-transform' : MOZpointingRight
 			});
 	}
 })
