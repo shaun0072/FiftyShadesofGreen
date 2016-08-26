b734336d86c62b147fc95d2bdd4b9a42b3e87edb
Shaun Russell <shaundavidrussell@yahoo.com>
2016-08-26 11:23:49 -0500

    Fixed submenu arrow animation for Safari

--- js/nav-slidedown.js
+++ js/nav-slidedown.js
@@ -5,8 +5,9 @@ $('.submenu-icon').on('click', function() {
 		MOZpointingRight = 'matrix(0, -1, 1, 0, 0, 0)',
 		pointingDown = 'matrix(-1.83697e-16, -1, 1, -1.83697e-16, 0, 0)',
 		$svgClicked = $submenu.siblings('div.tab-cont').find('svg'),
-	    $transformValue = $svgClicked.css('transform');
+	    $transformValue = $svgClicked.css('-webkit-transform');
 		$MOZtransformValue = $svgClicked.css('-moz-transform');
+		$
 
 	$('.submenu').each(function() {
 				
@@ -16,7 +17,7 @@ $('.submenu-icon').on('click', function() {
 		if( $thisSubmenu !== $clickedsubmenu ) {
 			$(this).slideUp();
 			$(this).siblings('div.tab-cont').find('svg').css({
-				'transform'      : pointingRight,
+				'-webkit-transform'      : pointingRight,
 				'-moz-transform' : MOZpointingRight
 				})
 		}
@@ -29,12 +30,12 @@ $('.submenu-icon').on('click', function() {
 	if( pointingRight === $transformValue || MOZpointingRight === $MOZtransformValue) {
 		
 		$transformValue = $svgClicked.css({
-			'transform'      : pointingDown,
+			'-webkit-transform'      : pointingDown,
 			'-moz-transform' : 'rotate(360deg)'
 			});
 	} else {
 		$transformValue = $svgClicked.css({
-			'transform'      : pointingRight,
+			'-webkit-transform'      : pointingRight,
 			'-moz-transform' : MOZpointingRight
 			});
 	}
