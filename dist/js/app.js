c896a5536ac6ec0d059a2d959be328dc0e7e34cd
Shaun Russell <shaundavidrussell@yahoo.com>
2016-08-27 20:36:01 -0500

    add mobile layout1

--- dist/js/app.js
+++ dist/js/app.js
@@ -51,22 +51,45 @@ $('.submenu-icon').on('click', function() {
 			});
 	}
 })
-$('.nav_toggle_btn').on('click', function() {
-	var $navigationStatus = $('.nav-outer-cont').css('margin-left'),
-		$pointingRight = 'matrix3d(-0.866025, 0, -0.5, 0, 0, 1, 0, 0, 0.5, 0, -0.866025, 0, 0, 0, 0, 1)';
+var windowWidth = $(window).width();
+
+if(windowWidth > 768){
+
+	$('.nav_toggle_btn').on('click', function() {
+		var $navigationStatus = $('.nav-outer-cont').css('margin-left'),
+			$pointingRight = 'matrix3d(-0.866025, 0, -0.5, 0, 0, 1, 0, 0, 0.5, 0, -0.866025, 0, 0, 0, 0, 1)';
+			
+		if( $navigationStatus === '0px') {
 		
-	if( $navigationStatus === '0px') {
-	
-		$('.nav-outer-cont').css('margin-left', '-225px');
-		$('.nav_toggle_btn .arrow').css('transform', $pointingRight);
-		$('.mainarea').css('margin-left', '0px');
-	} else {
+			$('.nav-outer-cont').css('margin-left', '-225px');
+			$('.nav_toggle_btn .arrow').css('transform', $pointingRight);
+			$('.mainarea').css('margin-left', '0px');
+		} else {
+			
+			$('.nav-outer-cont').css('margin-left', '0px');
+			$('.nav_toggle_btn .arrow').css('transform', 'rotateY(0deg)');
+			$('.mainarea').css('margin-left', '225px');
+		}
+	})
+
+}
+
+if(windowWidth < 768) {
+	$('.nav_toggle_btn').on('click', function() {
+		var $navigationStatus = $('.nav-outer-cont').css('margin-left'),
+			$pointingRight = 'matrix3d(-0.866025, 0, -0.5, 0, 0, 1, 0, 0, 0.5, 0, -0.866025, 0, 0, 0, 0, 1)';
+			
+		if( $navigationStatus === '0px') {
 		
-		$('.nav-outer-cont').css('margin-left', '0px');
-		$('.nav_toggle_btn .arrow').css('transform', 'rotateY(0deg)');
-		$('.mainarea').css('margin-left', '225px');
-	}
-})
+			$('.nav-outer-cont').css('margin-left', '-225px');
+			$('.nav_toggle_btn .arrow').css('transform', $pointingRight);
+		} else {
+			
+			$('.nav-outer-cont').css('margin-left', '0px');
+			$('.nav_toggle_btn .arrow').css('transform', 'rotateY(0deg)');
+		}
+	})
+}
 /*
      _ _      _       _
  ___| (_) ___| | __  (_)___
